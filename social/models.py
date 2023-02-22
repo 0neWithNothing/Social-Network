14f669581a75a39ee53ea361ac06cedf83a5cc69
YaNeShizoid <danyshilov5@gmail.com>
2023-02-22 18:38:33 +0300

    make model User and Friend request and first migration

--- social/models.py
+++ social/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractBaseUser
 # Create your models here.
+
+class User(AbstractBaseUser):
+    friends = models.ManyToManyField("User", blank=True)
+
+
+class FriendRequest(models.Model):
+    from_user = models.ForeignKey(User, related_name="from_user", on_delete=models.CASCADE)
+    to_user = models.ForeignKey(User, related_name="to_user", on_delete=models.CASCADE)
+
+
