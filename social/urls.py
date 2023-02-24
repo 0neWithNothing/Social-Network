4251fc6bd40444931839b71decfd6db6c300206c
YaNeShizoid <danyshilov5@gmail.com>
2023-02-24 15:58:35 +0300

    make creation and authentication user

--- social/urls.py
+++ social/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.HomeView.as_view()),
+    path('', views.HomeView.as_view(), name='home'),
+    path('signup/', views.SignupUserView.as_view(), name='signup'),
+    path('login/', views.LoginUserView.as_view(), name='login'),
+    path('logout/', views.LogoutUserView.as_view(), name='logout')
 ]
--- social/views.py
+++ social/views.py
@@ -1,8 +1,47 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+# from django.urls import reverse
+from django.http import HttpResponseRedirect
 from django.views.generic.base import TemplateView
+from django.views.generic.edit import CreateView
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.forms import AuthenticationForm
+
+from .models import User
+from .forms import CustomUserCreationForm
 
 # Create your views here.
 
 class HomeView(TemplateView):
     template_name = "social/home.html"
 
+
+class SignupUserView(CreateView):
+    form_class = CustomUserCreationForm
+    template_name = "social/signup_user.html"
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return HttpResponseRedirect('')
+
+
+class LoginUserView(LoginView):
+    template_name = "social/login_user.html"
+    authentication_form = AuthenticationForm
+    next_page = "home"
+
+
+class LogoutUserView(LogoutView):
+    template_name = "social/login_user.html"
+
+
+
+
+def send_friend_request(request, id):
+    pass
+
+
+def accept_friend_request(request, id):
+    pass
