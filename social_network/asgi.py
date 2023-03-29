b9c1df88ad9c5cd6932f78fd8be2515fbe40218e
YaNeShizoid <danyshilov5@gmail.com>
2023-03-29 17:19:23 +0300

    make new app chat and first configuring

--- social_network/asgi.py
+++ social_network/asgi.py
@@ -1,6 +1,8 @@
 import os
 
-from channels.routing import ProtocolTypeRouter
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'social_network.settings')
@@ -8,8 +10,14 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'social_network.settings')
 # is populated before importing code that may import ORM models.
 django_asgi_app = get_asgi_application()
 
-application = ProtocolTypeRouter({
-    "http": django_asgi_app,
-    # Just HTTP for now. (We can add other protocols later.)
-})
+import chat.routing
+
+application = ProtocolTypeRouter(
+    {
+        "http": django_asgi_app,
+        "websocket": AllowedHostsOriginValidator(
+            AuthMiddlewareStack(URLRouter(chat.routing.websocket_urlpatterns))
+        ),
+    }
+)
 
--- social_network/settings.py
+++ social_network/settings.py
@@ -78,6 +78,15 @@ TEMPLATES = [
 WSGI_APPLICATION = 'social_network.wsgi.application'
 ASGI_APPLICATION = "social_network.asgi.application"
 
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("127.0.0.1", 6379)],
+        },
+    },
+}
+
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
